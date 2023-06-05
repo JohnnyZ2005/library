894fca103ab13c285f122b42ac899549970f66b0
Oduoye Ayomide <oduoyeayomide6@gmail.com>
2023-06-05 19:12:15 +0100

    Enabled opening and closing of form

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // getting all the necessary elements 
 const addBookButton = document.querySelector('.add-book-button');
 const overlay = document.querySelector('#overlay')
+const cancelButton = document.querySelector('.cancel-button')
+
+//* Adding event listeners for all the neccesssary elements
 addBookButton.addEventListener('click', showForm)
+cancelButton.addEventListener('click', cancelForm)
+
 
 function showForm() {
     return overlay.style.display = 'block';
+}
+
+function cancelForm() {
+    return overlay.style.display = 'none';
 }
